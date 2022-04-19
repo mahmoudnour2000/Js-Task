ea7a3c6880aa82684e3b896a658caceb84c89a1d
mahmoud <mahmoudnour2000@git.com>
2022-04-19 05:54:46 +0200

    n

---
+++ task.js
@@ -0,0 +1,21 @@
+// Arry
+const cars = ["Saab", "Volvo", "BMW"];
+console.log (`Hello ${cars[2]}`);
+
+//Function
+const x = function (a, b) {return a * b};
+let z = x(4, 3);
+console.log(z);
+
+//loop
+let i = 5;
+for (let i = 0; i < 10; i++) {}
+console.log(i);
+
+//Global scope
+var m = "BMW";
+let n = "Volvo";  
+let s = "Saab";  
+console.log(m);
+console.log(n);
+console.log(s);
